c4eb84a87d00645d845fcc97d38862ae9aa12293
Jay1234555 <jayeshratwadkar@gmail.com>
2026-09-06 13:29:02 +0530

    Add MySQL database backup

---
+++ ims.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE company_management_db;
+
+use company_management_db;
+SELECT * FROM services;
+SELECT * FROM projects;
+SELECT * FROM customer_groups;
+SELECT * FROM customer_groups;
+
+SELECT name, is_deleted
+FROM customer_groups;
+
+
+INSERT INTO users (name, email, password, role)
+VALUES ('Admin', 'admin@gmail.com', 'admin123', 'ADMIN');
+
+INSERT INTO users (name, email, password, role)
+VALUES ('Test User', 'user@gmail.com', 'user123', 'USER');
+SELECT * FROM customer_groups;
